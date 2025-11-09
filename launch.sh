5d9e5e02b42b1d8d9b001f37ae30f8cbb96d1cac
Aldo <alidinicontini@gmail.com>
2025-11-09 17:46:10 -0600

    removed direction here

--- launch.sh
+++ launch.sh
@@ -1,28 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Args:
 AUDIOFILE=$1
 TEST_FLAG=$2
 
 # Path to this project
-PATH_PROJECT="/root/odio_stream_calls"
-
-# Determine the call direction based on the file name
-if [[ "$AUDIOFILE" == *"-in.wav" ]]; then
-    DIRECTION="inbound"
-elif [[ "$AUDIOFILE" == *"-out.wav" ]]; then
-    DIRECTION="outbound"
-else
-    echo "Error: the audio file must end with -in.wav or -out.wav"
-    exit 1
-fi
+PATH_PROJECT="/usr/local/bin/odio_stream_calls"
+echo "event_started" >> "/usr/local/bin/odio_stream_calls/connection.log"
+echo "venv: source $PATH_PROJECT/venv/bin/activate" >> "/usr/local/bin/odio_stream_calls/connection.log"
 
 source "$PATH_PROJECT/venv/bin/activate"
-
 # Run the Python script with the correct arguments
 # The --test flag is passed only if provided
 #echo "python3 "$PATH_PROJECT/stream_socket.py" "$AUDIOFILE" "$DIRECTION" $TEST_FLAG"
-python3 "$PATH_PROJECT/stream_socket.py" "$AUDIOFILE" "$DIRECTION" $TEST_FLAG
+echo "command python3 '$PATH_PROJECT/stream_socket.py' '$AUDIOFILE' $TEST_FLAG" >> "/usr/local/bin/odio_stream_calls/connection.log"
+$PATH_PROJECT/venv/bin/python3 "$PATH_PROJECT/stream_socket.py" "$AUDIOFILE" $TEST_FLAG
 
 # finish
 deactivate
